1129ccabe5b30321395cfbad7b277212f34ca6b4
Manoj <manojprodev@gmail.com>
2023-02-10 15:27:08 +0530

    Create-React-App

---
+++ Shopping/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className='App-header'>
+        <a href='/'>Shopping</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
